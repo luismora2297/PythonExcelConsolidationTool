714b2195aa2d755ea8cae307c4a787e2e9f04ab7
luismora2297 <luismora1997@gmail.com>
2022-05-24 23:06:01 -0600

    Tercer prueba y subida, reelaborando unos errores cometidos desde la primera version

--- ObserverExec/Observer.py
+++ ObserverExec/Observer.py
@@ -11,15 +11,29 @@ class FExcelHandler(FileSystemEventHandler):
 
     #Variables
     WorkDirectory = os.path.abspath('') #Manera dinamica de obtener el directorio actual
-    WorkExcelFiles = os.listdir(WorkDirectory) #Lista de los documentos en el directorio
+    WorkExcelFiles = [ xcelFiles for xcelFiles in os.listdir(WorkDirectory) if os.path.isfile(xcelFiles) and 'Observer.py' not in xcelFiles ]  #Lista de los documentos en el directorio filtrando que los documentos no sean carpetas y sin filtrar el documento principal del programa
 
-    #Funcion para los eventos de creación, modificacion, y otros
+    #Funciones para los eventos de creación, modificacion, y otros
+    def watchAnyEvent(self, directoryEvent): #Revisa cualquier evento no contemplado por las otras funciones en el directorio
+        print(directoryEvent.event_type, directoryEvent.src_path)
 
-    #Función para tener los nombres
+    def watchOnCreate(self, directoryEvent): #Revisa en eventos de creacion
+        print("Created ", directoryEvent.src_path)
+
+    def watchOnDelete(self, directoryEvent): #Revisa en eventos de borrado
+        print("Deleted ", directoryEvent.src_path)
+    
+    def watchOnModify(self, directoryEvent): #Revisa en eventos de modificacion
+        print("Modified ", directoryEvent.src_path)
+
+    def watchOnMove(self, directoryEvent): #Revisa en eventos de mover
+        print("Moved ", directoryEvent.src_path)
+
+    #Función inicial
     def __init__(self):
-        #Print Curren Location as Static
+        #Print Current Location as Static
         print("Your Current Working Directory is:"+ self.WorkDirectory)
-        print("The Current Files that your directory have are:")
+        print("\n The Current Files that your directory have are:")
         print(self.WorkExcelFiles)
 
         #Revisar cuantos archvios de Excel tenemos en el directorio
@@ -33,14 +47,20 @@ class FExcelHandler(FileSystemEventHandler):
             
             #Condicion dependiendo de la cantidad de Excels
             if xQuantity >= 1:
-                print("You have "+ xQuantity +" file(s) available to process")
+                print("\n You have "+ str(xQuantity) +" file(s) available to process")
             else:
-                print("You don't have any files available to process")
+                print("\n You don't have any files available to process")
 
         #Test The File
         FECheck(self.WorkExcelFiles)
 
-
-testVar = FExcelHandler()
-        
-
+#Main Executioner
+eventExecution = FExcelHandler()
+xcelObserver = Observer()
+xcelObserver.schedule(eventExecution, '.', recursive=False)
+#Try Catch para el evento de cerrar la ejecucion del programa
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    xcelObserver.stop()
